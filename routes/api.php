b9e5a1e4ce8158594d28bafbc2fb0f2191c502b7
AleksandraMrv <aleksandrasaki98@gmail.com>
2021-05-10 23:57:36 +0200

    drugi komit

--- routes/api.php
+++ routes/api.php
@@ -14,11 +14,7 @@ use App\Http\Controllers\API\FacultyController;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-    GET /api/universities - vrati mi sve univerzitete iz baze - index
-    GET /api/universities/{id} - vrati mi univerzitet sa datim id -jem - show
-    POST /api/universities - kreiraj novi univerzitet - store
-    PUT /api/universities/{id} - izmeni univerzitet sa datim id -jem podacima iz tela zahteva - update
-    DELETE /api/universities/{id} - obrisi univerzitet sa datim id -jem - destroy
+   
 */
 
 
